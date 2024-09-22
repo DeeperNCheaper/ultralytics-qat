03eebd250fdbd6372bff81f1f45012d45f6bf763
DeeperNCheaper <smallndeep@gmail.com>
2024-09-22 18:41:13 +0900

    init: add qdq node and modify qdq node

---
+++ ultralytics/engine/quantizer.py
@@ -0,0 +1,14 @@
+class Quantizer:
+    def __init__(self):
+        pass
+    
+    def run(self):
+        return
+
+
+class QDQ_GraphSurgeon:
+    def __init__(self):
+        pass
+    
+    def run(self):
+        pass
